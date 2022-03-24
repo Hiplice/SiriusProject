e3da1b5a50e44636e7f315579b8b50d6ccc2bdb3
Hiplice <tetant0n@gmail.com>
2022-03-24 12:47:36 +0300

    Designed login form

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+
+class User(auth_models.User):
+    banks = models.ForeignKey('dashboard.BankUser', on_delete=models.CASCADE)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    sum = models.SmallIntegerField()
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+
+
+class BankAccount(models.Model):
+    account_name = models.CharField(max_length=20)
+    sum = models.SmallIntegerField()
+    transactions = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+
+
+class BankUser(models.Model):
+    accounts = models.ForeignKey('BankAccount', on_delete=models.CASCADE)
+    bank_name = models.CharField(max_length=20)
