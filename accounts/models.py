0c2c38fe73e53dd462b155ef348e0c2b06e1db97
Hiplice <tetant0n@gmail.com>
2022-03-24 13:56:03 +0300

    Fixed users

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth import models as auth_models
 
 
-class User(auth_models.User):
-    banks = models.ForeignKey('dashboard.BankUser', on_delete=models.CASCADE)
+class User(auth_models.AbstractUser):
+    banks = models.ForeignKey('dashboard.BankUser', on_delete=models.CASCADE, null=True)
